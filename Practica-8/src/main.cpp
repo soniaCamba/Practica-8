082247b85180e2af156bfc0910ea2dcee28d0101
soniaCamba <104383054+soniaCamba@users.noreply.github.com>
2022-06-02 14:38:15 +0200

    Add files via upload

---
+++ Practica-8/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+ 
+ 
+void setup() {
+ 
+ Serial.begin(115200);
+  Serial2.begin(115200);
+}
+ 
+void loop() { //Choose Serial1 or Serial2 as required
+ while (Serial.available()) {
+   Serial2.print(char(Serial.read()));
+ }
+ while (Serial2.available()) {
+   Serial.print(char(Serial2.read()));
+ }
+}
